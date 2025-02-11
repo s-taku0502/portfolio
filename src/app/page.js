c31fdf34ecca1647f3482383326d987d8db0c396
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-02-12 01:29:52 +0900

    update:main部分を追加

--- src/app/page.js
+++ src/app/page.js
@@ -1,51 +1,17 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="flex items-center space-x-4">
-          <Image
-            className="dark:invert"
-            src="/sudo_for_clients.webp"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            // priority
-          />
-          <span className="text-lg">aiueo</span>
-        </div>
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            あいうえお
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <h1 className="text-4xl ts-2xl">sudo</h1>
+        <div className="min-h-screen p-8">
+          <h1 className="text-4xl mb-8 bg-blue-100 rounded-md">Welcome to My Portfolio</h1>
+          <nav className="flex space-x-4">
+            <Link href="/about" className="text-blue-600 hover:underline">
+              自己紹介ページへ
+            </Link>
+          </nav>
         </div>
       </main>
       <footer className="flex flex-col gap-4 row-start-3 items-center">
