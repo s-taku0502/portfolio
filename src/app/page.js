04f9205b8a7cc8ee00732fbb306a7142742b15fd
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-02-13 01:22:24 +0900

    update:ナビゲーションを編集

--- src/app/page.js
+++ src/app/page.js
@@ -8,14 +8,20 @@ export default function Home() {
       <div className="background-shape shape-bottom-left" />
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <h1 className="text-4xl ts-2xl">sudo</h1>
+          <h1 className="text-8xl ts-2xl">sudo</h1>
           <div className="min-h-screen p-8">
             <h1 className="text-4xl mb-8 bg-blue-100 rounded-md">
               Welcome to My Portfolio
             </h1>
             <nav className="flex space-x-4">
-              <Link href="/about" className="text-blue-600 hover:underline">
+              <Link href="/about" className="text-blue-600 ts:bold hover:underline">
                 自己紹介ページへ
+              </Link>
+              <Link href="/projects" className="text-blue-600 hover:underline">
+                制作物一覧へ
+              </Link>
+              <Link href="/contact" className="text-blue-600 hover:underline">
+                お問い合わせページへ
               </Link>
             </nav>
           </div>
