191875048492ae1c20efb95803f7da2e2d29a3bb
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-02-13 19:04:41 +0900

    fix:{welcome}がナビゲーションと被るのを阻止

--- src/app/page.js
+++ src/app/page.js
@@ -10,14 +10,14 @@ export default function Home() {
       <div className="background-shape shape-bottom-left" />
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="font-playfair flex flex-col gap-8 row-start-2 items-center text-center">
-          <h1 className="font-playfair text-[7vw] mb-2 rounded-md"> {/*`テキストサイズを画面の７％に設定`*/}
+          <h1 className="font-playfair text-[7vw] mb-2 rounded-md"> {/*text-[7vw]: テキストサイズを画面の７％に設定*/}
             Sudo`s portfolio
           </h1>
           <div className="max-h-screen p-8 w-full">
-            <h1 className="font-playfair text-[3vw] text-2xl mb-2 rounded-md">
+            <h1 className="font-playfair text-[3vw] text-2xl mb-6 rounded-md">
               Welcome to My Portfolio
             </h1>
-            <nav className="flex space-x-4 text-base md:text-[2vw] justify-center">
+            <nav className="flex space-x-4 text-base md:text-[2vw] justify-center leading-tight">
               <CustomLink href="/about">自己紹介ページへ</CustomLink>
               <CustomLink href="/projects">制作物一覧へ</CustomLink>
               <CustomLink href="/contact">お問い合わせページへ</CustomLink>
