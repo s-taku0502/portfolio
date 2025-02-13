15ec50b9f27e7a80b571c70ee3105a0395cc3bef
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-02-13 18:41:34 +0900

    update:文字を画面比に依存しないように設定

--- src/components/CustomFooter.js
+++ src/components/CustomFooter.js
@@ -2,21 +2,18 @@ import CustomLink from "./CustomLink";
 
 const CustomFooter = ({ children, className = "" }) => {
     return (
-        <footer className="font-playfair flex flex-col gap-4 row-start-3 items-center">
+        <footer className="font-playfair flex text-[1.2vw] flex-col gap-4 row-start-3 items-center">
             <div className="flex space-x-4">
-                <a className="font-jetbrains-mono text-center text-xl">©2025 </a>
-                <a className="text-center text-xl">sudo</a>
+                <a className="font-jetbrains-mono text-[1.2vw] text-center text-xl">©2025 </a>
+                <a className="text-center text-[1.2vw] text-xl">sudo</a>
             </div>
             <div className="flex space-x-4">
                 <CustomLink href="/privacy" >
                     プライバシーポリシー
                 </CustomLink>
-                <a
-                    href="https://x.com/ocean_t_umi"
-                    className="text-blue-600 hover:underline"
-                >
+                <CustomLink href="https://x.com/ocean_t_umi" >
                     X（旧Twitter）
-                </a>
+                </CustomLink>
             </div>
         </footer>
     );
