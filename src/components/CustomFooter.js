e0e655977410c614d07f002eb7183b0e0200742a
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-02-14 01:09:46 +0900

    fix:クラスネームの内容を調整

--- src/components/CustomFooter.js
+++ src/components/CustomFooter.js
@@ -6,12 +6,17 @@ const CustomFooter = ({ children, className = "" }) => {
             <div className="flex space-x-4">
                 <a className="font-playfair text-base md:text-[2vw] text-center">©2025 sudo</a>
             </div>
-            <div className="flex space-x-4">
-                <CustomLink href="/privacy" >
-                    プライバシーポリシー
-                </CustomLink>
-                <CustomLink href="https://x.com/ocean_t_umi" >
+            <div className="flex space-x-4 pb-10px">
+                    <CustomLink href="/privacy" className="bg-gray-200 rounded-lg p-2">
+                        プライバシーポリシー
+                    </CustomLink>
+            </div>
+            <div>
+                <CustomLink href="https://x.com/ocean_t_umi" className="bg-gray-200 rounded-lg p-1">
                     X（旧Twitter）
+                </CustomLink>
+                <CustomLink href="https://github.com/s-taku0502" className="bg-gray-200 rounded-lg p-1">
+                    GitHub
                 </CustomLink>
             </div>
         </footer>
