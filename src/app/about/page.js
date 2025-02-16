c4f3c6129a742943c88cce8d1377b682ec9c7372
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-02-16 23:47:17 +0900

    change:json形式に変更

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,6 +2,23 @@ import CustomH2 from "@/components/CustomH2";
 import "../../styles/globals.css";
 import CustomFooter from "@/components/CustomFooter";
 
+const aboutData = {
+  name: "Sudo Takumi",
+  affiliation: "Kanazawa Institute of Technology",
+  location: "Kanazawa, Japan",
+  motto: "Embrace challenges and grow through learning.",
+  hobbies: ["Programming", "Playing the guitar", "Traveling", "Reading books"],
+  technicalSkills: ["JavaScript", "Ruby", "Next.js", "Tailwind CSS", "Firebase"],
+  projects: {
+    description: "Check out my GitHub for some of my projects.",
+    link: "https://github.com"
+  },
+  socialMedia: [
+    { platform: "Twitter", link: "https://twitter.com" },
+    { platform: "LinkedIn", link: "https://www.linkedin.com" }
+  ]
+};
+
 export default function AboutPage() {
   return (
     <>
@@ -9,17 +26,63 @@ export default function AboutPage() {
       <div className="background-shape shape-bottom-left" />
       <main className="p-6">
         <h1 className="text-3xl font-bold mb-4">自己紹介ページへようこそ</h1>
-        <p className="text-lg">
-          Hello! I'm a passionate developer with experience in technologies like JavaScript, Ruby, and Firebase.
-          I enjoy building creative solutions and learning new technologies.
-        </p>
+
         <div>
           <CustomH2>Name</CustomH2>
-          <p className="text-lg">Sudo Takumi</p>
+          <p className="text-lg">{aboutData.name}</p>
         </div>
+
         <div>
           <CustomH2>Affiliation</CustomH2>
-          <p className="text-lg">Kanazawa Institute of Technology</p>
+          <p className="text-lg">{aboutData.affiliation}</p>
+        </div>
+
+        <div>
+          <CustomH2>Location</CustomH2>
+          <p className="text-lg">{aboutData.location}</p>
+        </div>
+
+        <div>
+          <CustomH2>Motto</CustomH2>
+          <p className="text-lg">"{aboutData.motto}"</p>
+        </div>
+
+        <div>
+          <CustomH2>Hobbies</CustomH2>
+          <p className="text-lg">{aboutData.hobbies.join(", ")}</p>
+        </div>
+
+        <div>
+          <CustomH2>Technical Skills</CustomH2>
+          <ul className="list-disc pl-6">
+            {aboutData.technicalSkills.map((skill, index) => (
+              <li key={index}>{skill}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <CustomH2>Projects</CustomH2>
+          <p className="text-lg">
+            {aboutData.projects.description}{" "}
+            <a href={aboutData.projects.link} className="text-blue-600">
+              GitHub
+            </a>
+          </p>
+        </div>
+
+        <div>
+          <CustomH2>Social Media</CustomH2>
+          <p className="text-lg">
+            {aboutData.socialMedia.map((social, index) => (
+              <span key={index}>
+                <a href={social.link} className="text-blue-600">
+                  {social.platform}
+                </a>
+                {index < aboutData.socialMedia.length - 1 && " and "}
+              </span>
+            ))}
+          </p>
         </div>
       </main>
       <CustomFooter className="item-align-bottom" />
