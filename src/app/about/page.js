b1c1732a25f9c7f0f0160de9bd04addcca0d10d6
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-02-16 14:32:21 +0900

    add:背景模様の追加

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,12 +1,18 @@
+import "../../styles/globals.css";
+import CustomFooter from "@/components/CustomFooter";
+
 export default function AboutPage() {
-    return (
+  return (
+    <>
+      <div className="background-shape shape-top-right" />
+      <div className="background-shape shape-bottom-left" />
       <main className="p-6">
-        <h1 className="text-3xl font-bold mb-4">About Me</h1>
+        <h1 className="text-3xl font-bold mb-4">自己紹介ページへようこそ</h1>
         <p className="text-lg">
           Hello! I'm a passionate developer with experience in technologies like JavaScript, Ruby, and Firebase.
           I enjoy building creative solutions and learning new technologies.
         </p>
       </main>
-    );
-  }
-  
+    </>
+  );
+}
