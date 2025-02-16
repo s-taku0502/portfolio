bbf3a37638ae45ad3d07201f663f2ddc8be17444
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-02-16 14:30:08 +0900

    update:可読性の向上

--- src/components/CustomFooter.js
+++ src/components/CustomFooter.js
@@ -2,20 +2,18 @@ import CustomLink from "./CustomLink";
 
 const CustomFooter = ({ children, className = "" }) => {
     return (
-        <footer className="font-playfair flex text-base md:text-[2vw] flex-col gap-4 row-start-3 items-center">
-            <div className="flex space-x-4">
-                <a className="font-playfair text-base md:text-[2vw] text-center">©2025 sudo</a>
+        <footer className="font-playfair flex flex-col gap-4 row-start-3 items-center">
+            <div className="flex">
+                <a className="font-playfair text-base md:text-[1.3vw] text-center">©2025 sudo</a>
             </div>
-            <div className="flex space-x-4 pb-10px">
-                    <CustomLink href="/privacy" className="bg-gray-200 rounded-lg p-2">
-                        プライバシーポリシー
-                    </CustomLink>
-            </div>
-            <div>
-                <CustomLink href="https://x.com/ocean_t_umi" className="bg-gray-200 rounded-lg p-1">
+            <div className="flex">
+                <CustomLink href="/privacy" className="text-base md:text-[1.2vw] bg-gray-200 rounded-md">
+                    プライバシーポリシー<br />
+                </CustomLink>　
+                <CustomLink href="https://x.com/ocean_t_umi" className="text-base md:text-[1.2vw] bg-gray-200 rounded-md">
                     X（旧Twitter）
-                </CustomLink>
-                <CustomLink href="https://github.com/s-taku0502" className="bg-gray-200 rounded-lg p-1">
+                </CustomLink>　
+                <CustomLink href="https://github.com/s-taku0502" className="text-base md:text-[1.2vw] bg-gray-200 rounded-md">
                     GitHub
                 </CustomLink>
             </div>
