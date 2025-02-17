e140cd1287452b388c2fdbc4cf9b7fd2d2884cd5
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-02-18 01:18:46 +0900

    add:ヘッダーの作成

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 // import Link from "next/Link";
+import Header from "@/components/Header";
 import "../styles/globals.css";
 import CustomLink from "@/components/CustomLink";
 import CustomFooter from "@/components/CustomFooter";
@@ -6,6 +7,7 @@ import CustomFooter from "@/components/CustomFooter";
 export default function Home() {
   return (
     <>
+      <Header />
       <div className="background-shape shape-top-right" />
       <div className="background-shape shape-bottom-left" />
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -17,11 +19,10 @@ export default function Home() {
             <h1 className="font-playfair text-[3vw] text-2xl mb-6 rounded-md">
               Welcome to My Portfolio
             </h1>
-            <nav className="flex space-x-4 text-base md:text-[2vw] justify-center leading-tight">
-              <CustomLink href="/about">自己紹介ページへ</CustomLink>
-              <CustomLink href="/projects">制作物一覧へ</CustomLink>
-              <CustomLink href="/contact">お問い合わせページへ</CustomLink>
-            </nav>
+            <p className="text-lg">
+              このサイトは、私のポートフォリオサイトです。私のスキルや経歴を紹介しています。  <br />
+              また、このサイトはNext.jsを使用して作成しています。
+            </p>
           </div>
         </main>
         <CustomFooter />
