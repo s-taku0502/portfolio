e37f01a3c28388a911b2d415eb101e7ad45d1cf0
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-02-18 16:58:15 +0900

    update:ヘッダー部分とボディ部分で高さを包括

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,30 @@
-// import Link from "next/Link";
 import Header from "@/components/Header";
 import "../styles/globals.css";
-import CustomLink from "@/components/CustomLink";
 import CustomFooter from "@/components/CustomFooter";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
       <div className="background-shape shape-top-right" />
       <div className="background-shape shape-bottom-left" />
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="font-playfair flex flex-col gap-8 row-start-2 items-center text-center">
-          <h1 className="font-playfair text-[7vw] mb-2 rounded-md"> {/*text-[7vw]: テキストサイズを画面の７％に設定*/}
-            Sudo`s portfolio
+
+      <main className="flex flex-col flex-grow items-center justify-center text-center p-8 sm:p-20">
+        <h1 className="font-playfair text-[7vw] mb-2 rounded-md">
+          Sudo`s portfolio
+        </h1>
+        <div className="p-8 w-full">
+          <h1 className="font-playfair text-[3vw] text-2xl mb-6 rounded-md">
+            Welcome to My Portfolio
           </h1>
-          <div className="max-h-screen p-8 w-full">
-            <h1 className="font-playfair text-[3vw] text-2xl mb-6 rounded-md">
-              Welcome to My Portfolio
-            </h1>
-            <p className="text-lg">
-              このサイトは、私のポートフォリオサイトです。私のスキルや経歴を紹介しています。  <br />
-              また、このサイトはNext.jsを使用して作成しています。
-            </p>
-          </div>
-        </main>
-        <CustomFooter />
-      </div>
-    </>
+          <p className="text-lg">
+            このサイトは、私のポートフォリオサイトです。私のスキルや経歴を紹介しています。  <br />
+            また、このサイトはNext.jsを使用して作成しています。
+          </p>
+        </div>
+      </main>
+
+      <CustomFooter />
+    </div>
   );
 }
